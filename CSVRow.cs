95fb265e8392fa43a200ae618bc32e5e1425d3d4
Gabriele Bologna <bolognagabriele2003@gmail.com>
2022-08-25 10:52:44 +0200

    First commit

---
+++ CSVRow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class CSVRow
+{
+    public List<string> columns; // Parsed CSV columns of this specified CSV row.
+
+    /// <summary>
+    /// Main constructor of CSVRow. Simply declaring a row of the CSV file.
+    /// </summary>
+    public CSVRow()
+    {
+        this.columns = new List<string>();
+    }
+}
